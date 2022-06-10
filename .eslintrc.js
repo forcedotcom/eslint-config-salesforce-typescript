ed8179a66af4a14e2942734822486257c4d65c45
mshanemc <shane.mclaughlin@salesforce.com>
2022-06-10 11:48:51 -0500

    feat: check strings with methods

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,6 +65,7 @@ module.exports = {
     "@typescript-eslint/no-shadow": "error",
     '@typescript-eslint/prefer-for-of': 'error',
     '@typescript-eslint/prefer-function-type': 'error',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
     '@typescript-eslint/quotes': [
       'error',
       'single',

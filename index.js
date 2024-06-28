8b10c60eb0e47554f7e108cace5827d6bf78e809
mshanemc <shane.mclaughlin@salesforce.com>
2024-06-28 12:49:58 -0500

    refactor: correc file structure

--- index.js
+++ index.js
@@ -95,19 +95,17 @@ module.exports = {
   ignorePatterns: ['*.js'],
   overrides: [
     {
-      files: ['src/**'],
-      '@typescript-eslint/restrict-template-expressions': [
-        'error',
-        {
-          allowNullish: false,
-          allowBoolean: true,
-          allowNumber: true,
-        },
-      ],
-    },
-    {
+      // put stricter rules you only want to apply to production code here
       files: ['src/**'],
       rules: {
+        '@typescript-eslint/restrict-template-expressions': [
+          'error',
+          {
+            allowNullish: false,
+            allowBoolean: true,
+            allowNumber: true,
+          },
+        ],
         'import/no-extraneous-dependencies': [
           'error',
           {
